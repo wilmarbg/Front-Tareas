f61cdfb56a6dce1a1502f71545987040008fed34
wbeletzuy <wilmarbg773@gmail.com>
2024-08-21 19:15:58 -0600

    se cambio para que no fuera localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     //production: true,
     //APPLICATION:"admin",
-    API: 'http://localhost:3100'
+    //API: 'http://localhost:3100'
+    API: 'http://209.126.125.193:3100'
 };
